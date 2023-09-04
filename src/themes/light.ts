47b1441c96325ac919eba1ccef8327aa569feeaf
andradevs <guilhermeandradevs@gmail.com>
2023-09-04 19:36:38 -0300

    adding theme

---
+++ src/themes/light.ts
@@ -0,0 +1,7 @@
+export default {
+  primary: '#F0F2F5',
+  onPrimary: '#161513',
+  secundary: '#ffffff',
+  onSecundary: '#1C1C22',
+  gradient: 'linear-gradient(90deg, #b16cea 25%, #ff5e69 50%, #ff8a56 75%, #ffa84b 100%);',
+};
